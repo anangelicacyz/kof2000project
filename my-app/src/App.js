9771f4d109c409452e0dce8b902e32e4477a44f6
ana <anangeluxd@gmail.com>
2023-03-31 17:14:30 -0500

    mejora

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Menu from '../src/components/menu';
 import './App.css';
-import {BrowserRouter, Route, Routes, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Navbar from './components/navbar';
 import HeroDetails from './components/herodetails';
 
