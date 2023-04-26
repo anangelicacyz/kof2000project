01ab7ce1621a0c091fa9caf8e1d80f651eaa5c96
ana <anangeluxd@gmail.com>
2023-04-26 14:23:35 -0500

    organizacion

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Menu from '../src/components/menu';
+import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Navbar from './components/navbar';
+import HeroDetails from './components/herodetails';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+          <Route  path='/kof' element={<Menu/>} />
+          <Route exact path='/kof/:heroName' Component={HeroDetails} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+
+    
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className= 'navbar'>
+            <img src= './../assets/logo-kof-2000.png' />  
+        </div>
+    )
+}
+
+export default Navbar
